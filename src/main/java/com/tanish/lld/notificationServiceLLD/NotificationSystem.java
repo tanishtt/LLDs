6d522e93ec365390af510dbcedb929f6235d3ddc
Tanish Mohanta <tanishmohanta1901@gmail.com>
2026-08-23 23:50:15 +0530

    notification and decorator done in notification system

--- src/main/java/com/tanish/lld/notificationServiceLLD/NotificationSystem.java
+++ src/main/java/com/tanish/lld/notificationServiceLLD/NotificationSystem.java
@@ -1,5 +1,7 @@
 package com.tanish.lld.notificationServiceLLD;
 
+import java.time.LocalDateTime;
+
 /*
  * ============================================================
  * 1. DECORATOR PATTERN
@@ -21,6 +23,72 @@ package com.tanish.lld.notificationServiceLLD;
  * Dynamically add additional information/behavior to a
  * notification without modifying the original notification.
  */
+interface Notification{
+    String getContent();
+}
+
+class SimpleNotification implements Notification{
+
+    private final String textContent;
+
+    SimpleNotification(String textContent) {
+        this.textContent = textContent;
+    }
+
+    @Override
+    public String getContent() {
+        return this.textContent;
+    }
+}
+
+class HTMLNotification implements Notification{
+    private final String htmlContent;
+
+    HTMLNotification(String htmlContent) {
+        this.htmlContent = htmlContent;
+    }
+
+    @Override
+    public String getContent() {
+        return this.htmlContent;
+    }
+}
+
+abstract class NotificationDecorator implements Notification{
+    protected final Notification notification;
+
+    NotificationDecorator(Notification notification) {
+        this.notification = notification;
+    }
+
+    public String getContent(){
+        return notification.getContent();
+    }
+}
+
+class TimestampDecorator extends NotificationDecorator{
+
+    TimestampDecorator(Notification notification) {
+        super(notification);
+    }
+
+    public String getContent(){
+        return "["+ LocalDateTime.now()+"] "+notification.getContent();
+    }
+}
+class SignatureDecorator extends NotificationDecorator{
+    private final String signature;
+
+    SignatureDecorator(Notification notification, String signature,) {
+        super(notification);
+        this.signature = signature;
+    }
+
+    public String getContent() {
+
+        return notification.getContent() + "\n-- " + signature;
+    }
+}
 
 public class NotificationSystem {
 }
